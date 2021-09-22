56226c68b541c4df5ff9745b9065a1ed086ce8ed
Prerana <ingaleprerana30@gmail.com>
2021-09-22 17:49:47 +0530

    Sample Demo program

---
+++ corejavaproject/src/corejavaproject/HelloWorld.java
@@ -0,0 +1,31 @@
+package corejavaproject;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Hello World");
+       System.out.println("Hello prerana");
+       
+       byte floorNo=3;
+       short EId= 23;
+       long MNo= 23534657465768L;
+       float c= 3.6f;
+       int age= 24;
+       char e ='P';
+       double f =42.4353;
+       boolean g = true;
+       
+       System.out.println("Employee Id is:" +EId);
+       System.out.println("Mobile No is: " +MNo);
+       System.out.println(c);
+       System.out.println("Employee Age is: "+age);
+       System.out.println(e);
+       System.out.println(f);
+       System.out.println(g);
+       System.out.println("floorNo is :" +floorNo);
+       
+       
+	}
+
+}
---
+++ corejavaproject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Prerana
+ *
+ */
+module corejavaproject {
+}
